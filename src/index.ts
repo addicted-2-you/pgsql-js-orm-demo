b803b1eff3c9c8b7376882ca6550b6d79bba4976
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2023-06-10 17:38:44 +0200

    add basic knex demo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
+import knex from "knex";
 import dotenv from "dotenv";
 
+import getKnexConfig from "./get-knex-config";
+
+// put .env variables into process.env
 dotenv.config();
 
-console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
+const db = knex(getKnexConfig());
+
+// Example usage: Select all users from the 'users' table
+db.table("users")
+  .select()
+  .then((users) => {
+    console.log(users);
+  })
+  .finally(() => {
+    db.destroy();
+  });
