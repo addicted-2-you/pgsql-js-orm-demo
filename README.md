5768eb73b0b46f3289173b70b3a8acbc20d6d7c8
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-05-21 11:40:31 +0200

    some tweaks

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 1. `docker-compose up -d`
 2. `cd ./scripts`
-3. `npm run seed`
+3. `npm i`
+4. `npm run seed`
--- sql/delete-database.sql
+++ sql/delete-database.sql
@@ -3,9 +3,10 @@ DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS reactions_to_posts;
 DROP TABLE IF EXISTS reactions;
 DROP TABLE IF EXISTS posts;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS permissions;
-DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS role_permissions;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS user_permissions;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS permissions;
+DROP TABLE IF EXISTS roles;
+ 
