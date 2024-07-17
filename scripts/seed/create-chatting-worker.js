64f3e371699620b1b1b9ec41a8660e47fee7a5c2
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-07-17 19:17:32 +0200

    remove extra measuring

--- scripts/seed/create-chatting-worker.js
+++ scripts/seed/create-chatting-worker.js
@@ -1,11 +1,10 @@
 const { parentPort, workerData } = require("worker_threads");
 
 const { createChatting } = require("./generate-data");
-const { withTimeMeasureAsync } = require("./utils");
 
 async function main() {
   try {
-    const result = await withTimeMeasureAsync(createChatting)(workerData.users);
+    const result = await createChatting(workerData.users);
     parentPort.postMessage(result);
   } catch (error) {
     parentPort.postMessage({ error: error.message });
